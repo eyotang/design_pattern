b6245d6b7a40b01c32dd0b9a0e1727e7b73bf2ea
tangyq <tangyq@yoozoo.com>
2021-03-31 12:46:27 +0800

    单例更新

--- 01.singletion.py
+++ 01.singletion.py
@@ -4,26 +4,29 @@
 import re, sys, os, traceback, signal
 
 
-class MyBeautifulGirl(object):
-    """我的漂亮女神"""
+class Tony(object):
+    """程序员Tony"""
     __instance = None
     __isFirstInit = False
 
     def __new__(cls, name):
         if not cls.__instance:
-            MyBeautifulGirl.__instance = super().__new__(cls)
+            Tony.__instance = super().__new__(cls)
         return cls.__instance
 
     def __init__(self, name):
         if not self.__isFirstInit:
-            self.__name = name
+            self.__my_girl = name
+            Tony.__isFirstInit = True
+
+    def meet(self, name):
+        if name == self.__my_girl:
             print("遇见" + name + "，我一见钟情！")
-            MyBeautifulGirl.__isFirstInit = True
         else:
             print("遇见" + name + "，我置若罔闻！")
 
     def show_my_heart(self):
-        print(self.__name + "就是我心中的唯一！")
+        print(self.__my_girl + "就是我心中的唯一！")
 
 
 def on_signal_int(signum, frame):
@@ -38,11 +41,17 @@ def register_signal():
 def main():
     register_signal()
 
-    jenny = MyBeautifulGirl("Jenny")
-    jenny.show_my_heart()
-    kimi = MyBeautifulGirl("Kimi")
-    kimi.show_my_heart()
-    print("id(jenny):", id(jenny), " id(kimi):", id(kimi))
+    # 24岁的Tony
+    tony_24 = Tony("Jenny")
+    tony_24.meet("Jenny")
+    tony_24.show_my_heart()
+
+    # 26岁的Tony
+    tony_26 = Tony("Kimi")
+    tony_26.meet("Kimi")
+    tony_26.show_my_heart()
+
+    print("id(tony_24):", id(tony_24), " id(tony_26):", id(tony_26))
 
     return 0
 
